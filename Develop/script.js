1d137ecf912892c5e74c5838df8c80c4a639dc77
cullinans1  <sammy.cullinan@gmail.com>
2020-06-28 15:53:16 -0700

    time colors work except for current time

--- Develop/script.js
+++ Develop/script.js
@@ -1,31 +1,53 @@
-// Adding date to header
+$(document).ready(function() {
+
+    // Adding date to header
 var currentTime = moment().format("dddd MMMM Do, YYYY");
-var currentDay = $("#currentDay")
+$("#currentDay")
 .text("Today is " + currentTime);
-$("jumbotron").append(currentDay);
 
 // save button clicks and saving to local storage
 $(".saveBtn").on("click", function() {
-    //get values for text entry and time it is in
+    //get values of text entry and time it is in
     var taskText = $(this).siblings(".event-entry").val();
     var taskTime = $(this).parent().attr("id");
     //save items to local storage
     localStorage.setItem(taskTime, taskText);
 });
-// function to check current hour 
-function currentHour() {
-    var updatedHour = moment().format("LT");
-    console.log(updatedHour);
-};
-currentHour();
+// loop through each time slot to compare 
+function compareTimes() {
+    //compare current hour to hour of task
+var updatedHour = moment().hours();
+    $(".timeblock").each(function() {
+        var rowTime = $(this).attr("id");
+        var rowNum = parseInt(rowTime);
+        if(rowNum < updatedHour) {
+            $(this).addClass("pastHour");
+        }
+        else if (rowTime === updatedHour) {
+            $(this).removeClass("pastHour")
+            .addClass("currentHour");
+        }
+        else if (rowTime > updatedHour) {
+            $(this).removeClass("pastHour")
+            .removeClass("currentHour")
+            .addClass("futureHour");
+        }
+    });
+}
+compareTimes();
+
+//set interval to check times every 30 min
+setInterval(compareTimes, (1000*60)*30);
 
 //loading local storage items
-$("#9am .event-entry").val(localStorage.getItem("9am"));
-$("#10am .event-entry").val(localStorage.getItem("10am"));
-$("#11am .event-entry").val(localStorage.getItem("11am"));
-$("#12pm .event-entry").val(localStorage.getItem("12pm"));
-$("#1pm .event-entry").val(localStorage.getItem("1pm"));
-$("#2pm .event-entry").val(localStorage.getItem("2pm"));
-$("#3pm .event-entry").val(localStorage.getItem("3pm"));
-$("#4pm .event-entry").val(localStorage.getItem("4pm"));
-$("#5pm .event-entry").val(localStorage.getItem("5pm"));
+$("#9 .event-entry").val(localStorage.getItem("9"));
+$("#10 .event-entry").val(localStorage.getItem("10"));
+$("#11 .event-entry").val(localStorage.getItem("11"));
+$("#12 .event-entry").val(localStorage.getItem("12"));
+$("#13 .event-entry").val(localStorage.getItem("13"));
+$("#14 .event-entry").val(localStorage.getItem("14"));
+$("#15 .event-entry").val(localStorage.getItem("15"));
+$("#16 .event-entry").val(localStorage.getItem("16"));
+$("#17 .event-entry").val(localStorage.getItem("17"));
+
+});
